b2371d95fed2208a0ef8b3adf23b9c55d5860dc3
Tiavina3827 <hei.tiavina.4@gmail.com>
2024-09-24 13:47:38 +0300

    feat: Presentation component

---
+++ src/Component/Presentation.jsx
@@ -0,0 +1,22 @@
+import "../App.css";
+function Presentation(){
+    return(
+        <>
+        <div className="container">
+            <div className="text-part">
+                <div className="text">
+                    <h1 className="head-text">Optimiser votre </h1>
+                    <h1 className="head-text">entreprise </h1>
+                    <h1 className="head-text p-b-3">commence ici</h1>
+                    <p className="p-head">MA-ERI Consulting</p>
+                    <button className="head-button"> en savoir plus</button>
+                </div>
+
+            </div>
+            <div className="hero_image-1"></div>
+        </div>
+        </>
+    )
+}
+
+export default Presentation;
